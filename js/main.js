0ee48728eb0b43f13c67d3c0bcdde6d0935fd2f7
Lensom <Magura21@yandex.ua>
2017-10-19 15:44:47 +0300

    up2
    up

--- js/main.js
+++ js/main.js
@@ -78,3 +78,9 @@ $(this).removeClass("active");
 $(this).addClass("active");
 
 });
+
+$("body").on('click', '[href*="#"]', function(e){
+  var fixed_offset = 0;
+  $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+  e.preventDefault();
+});
